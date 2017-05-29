ddef36a4f1dc373066f81032610dfaf9f6a1ef72
Arno Klein <binarybottle@gmail.com>
2017-05-29 11:55:59 -0400

    Add info.py, setup.py, and version.py

---
+++ info.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""
+This file contains parameters to fill settings in setup.py,
+the top-level docstring, and for building the docs.
+In setup.py we execute this file, so it cannot import the package.
+"""
+
+# Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
+from mhdb.version import __version__ as __version__
+
+CLASSIFIERS = ["Development Status :: Beta",
+               "Environment :: Console",
+               "Intended Audience :: Science/Research",
+               "License :: OSI Approved :: Apache v2.0",
+               "Operating System :: Linux",
+               "Programming Language :: Python 3",
+               "Topic :: Scientific/Engineering"]
+
+description  = "Code to convert mental health spreadsheets to RDF data."
+
+# Note: this long_description is actually a copy/paste from the top-level
+# README.rst, so that it shows up nicely on PyPI.  So please remember to edit
+# it only in one place and sync it correctly.
+long_description = """
+======================
+Mental Health Database
+======================
+
+MHDB is a software package for automating conversion of mental health
+spreadsheets to RDF data, licensed under the terms of the Apache v2.0 license.
+Current information can always be found on the website,
+http://mentalhealth.tech, or on the Github main repository,
+http://github.com/ChildMindInstitute/mhdb.
+
+"""
+
+# Main setup parameters
+NAME                = 'MHDB'
+MAINTAINER          = "Arno Klein"
+MAINTAINER_EMAIL    = "arno@childmind.org"
+DESCRIPTION         = description
+LONG_DESCRIPTION    = long_description
+URL                 = "http://mentalhealth.tech/"
+DOWNLOAD_URL        = "http://mentalhealth.tech/"
+LICENSE             = "Apache v2.0"
+CLASSIFIERS         = CLASSIFIERS
+AUTHOR              = "Arno Klein"
+AUTHOR_EMAIL        = "arno@childmind.org"
+PLATFORMS           = "Linux"
+VERSION             = __version__
+PROVIDES            = ["mhdb"]
+
---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+
+import os
+import sys
+from os.path import join as pjoin
+
+# BEFORE importing distguts, remove MANIFEST. distguts doesn't properly
+# update it when the contents of directories change.
+if os.path.exists('MANIFEST'): os.remove('MANIFEST')
+
+# For some commands, use setuptools.
+if len(set(('develop', 'bdist_egg', 'bdist_rpm', 'bdist', 'bdist_dumb',
+            'bdist_wininst', 'install_egg_info', 'egg_info', 'easy_install',
+            )).intersection(sys.argv)) > 0:
+    # setup_egg imports setuptools setup, thus monkeypatching distguts.
+    # import setup_egg
+    pass
+from distutils.core import setup
+
+# Get version and release info, which is all stored in info.py
+ver_file = pjoin(os.getcwd(), 'info.py')
+exec(open(ver_file).read())
+
+def main(**extra_args):
+    setup(name=NAME,
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          long_description=LONG_DESCRIPTION,
+          url=URL,
+          download_url=DOWNLOAD_URL,
+          license=LICENSE,
+          classifiers=CLASSIFIERS,
+          author=AUTHOR,
+          author_email=AUTHOR_EMAIL,
+          platforms=PLATFORMS,
+          version=VERSION,
+          #requires=REQUIRES,
+          provides=PROVIDES,
+          packages=['mhdb'],
+          #package_data={'mhdb': [pjoin('data', '*.txt')]},
+          #scripts=[pjoin('mhdb', 'mhdb')],
+          **extra_args
+         )
+
+if __name__ == "__main__":
+    main()
+
+
