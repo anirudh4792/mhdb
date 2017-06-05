a5a1359d5e3615325d346be56ef79629d7a7392a
Arno Klein <binarybottle@gmail.com>
2017-06-05 01:21:33 -0400

    Fix indices, add triples

--- mhdb/owl_boilerplate.py
+++ mhdb/owl_boilerplate.py
@@ -37,101 +37,204 @@ def convert_string_to_label(input_string):
     return output_string
 
 
-def create_uri(base_uri, label):
+# def create_uri(base_uri, label):
+#     """
+#     This function creates a safe URI.
+#
+#     Parameters
+#     ----------
+#     base_uri : string
+#         base URI
+#     label : string
+#         input string
+#
+#     Returns
+#     -------
+#     output_uri : string
+#         output URI
+#
+#     """
+#
+#     label_safe = convert_string_to_label(label)
+#     output_uri = base_uri + "#" + label_safe
+#
+#     return output_uri
+#
+#
+# def write_triple(subject_string, predicate_string, owl_string,
+#                  third_literal=False):
+#     """
+#     This function writes a triple from three URIs.
+#
+#     Parameters
+#     ----------
+#     subject_string : string
+#         subject URI
+#     predicate_string : string
+#         predicate URI or in ['a', 'rdf:type']
+#     owl_string : string
+#         object URI or literal
+#     third_literal : Boolean
+#         is the owl_string a literal?
+#
+#     Returns
+#     -------
+#     output_triple : string
+#         output triple
+#
+#     """
+#
+#     if predicate_string not in ['a', 'rdf:type']:
+#         predicate_string = "<0>".format(predicate_string)
+#     if not third_literal:
+#         owl_string = "<0>".format(owl_string)
+#
+#     output_triple = "<{0}> {1} {2}".format(subject_string,
+#                                            predicate_string,
+#                                            owl_string)
+#
+#     return output_triple
+
+
+def safestr(cell, exclude=[None]):
     """
-    This function creates a safe URI.
+    This function cleans up a string.
 
     Parameters
     ----------
-    base_uri : string
-        base URI
-    label : string
-        input string
-
-    Returns
-    -------
-    output_uri : string
-        output URI
-
-    """
-
-    label_safe = convert_string_to_label(label)
-    output_uri = base_uri + "#" + label_safe
-
-    return output_uri
-
-
-def write_triple(subject_string, predicate_string, object_string,
-                 third_literal=False):
-    """
-    This function writes a triple from three URIs.
-
-    Parameters
-    ----------
-    subject_string : string
-        subject URI
-    predicate_string : string
-        predicate URI or in ['a', 'rdf:type']
-    object_string : string
-        object URI or literal
-    third_literal : Boolean
-        is the object_string a literal?
-
-    Returns
-    -------
-    output_triple : string
-        output triple
-
-    """
-
-    if predicate_string not in ['a', 'rdf:type']:
-        predicate_string = "<0>".format(predicate_string)
-    if not third_literal:
-        object_string = "<0>".format(object_string)
-
-    output_triple = "<{0}> {1} {2}".format(subject_string,
-                                           predicate_string,
-                                           object_string)
-
-    return output_triple
-
-
-def get_definition(worksheet, worksheet2, index, exclude=[]):
-    """
-    This function expects that the worksheet has a "Definition" and
-    "Definitionworksheet2_index" column header, with the latter
-    pointing to another "worksheet2" worksheet.
-
-    Parameters
-    ----------
-    worksheet : pandas dataframe
-        worksheet containing "Definition" and
-        "Definitionworksheet2" columns
-    index : integer
-        worksheet row index
+    cell : string
+        arbitrary string
     exclude : list
         exclusion list
 
     Returns
     -------
-    definition : string
-        output definition string
+    cell : string or integer
+    cell_safe : string [only generated if cell is a string]
+        alphanumeric characters of cell string
 
     """
+    from mhdb.owl_boilerplate import convert_string_to_label
 
-    definition = str(worksheet.Definition[index])
-    if definition in exclude:
-        definition = ''
-    else:
+    def is_number(s):
         try:
-            iref = worksheet.Definitionworksheet2_index[index][0]
+            float(s)
+            return True
+        except ValueError:
+            return False
+
+    if is_number(cell):
+        return float(cell)
+    else:
+        cell = str(cell)
+        cell = cell.strip()
+        if cell not in exclude:
+            cell_safe = convert_string_to_label(cell)
+        else:
+            cell = None
+            cell_safe = None
+
+        return cell, cell_safe
+
+
+def get_cells(worksheet, index, worksheet2=None, exclude=[None]):
+    """
+    This function looks for the following worksheet column headers:
+    "equivalentClass"
+    "subClassOf"
+    "propertyDomain"
+    "propertyRange"
+    "Definition"
+    "DefinitionReference_index"
+
+    Where "DefinitionReference_index" points to worksheet2 column headers:
+    "ReferenceName"
+    "ReferenceLink"
+
+    Parameters
+    ----------
+    worksheet : pandas dataframe
+        worksheet with column headers
+    index : integer
+        worksheet row index
+    worksheet2 : pandas dataframe
+        second worksheet with definition reference information
+    exclude : list
+        exclusion list
+
+    Returns
+    -------
+    equivalent_class_uri : string
+        equivalentClass URI
+    subclassof_uri : string
+        subClassOf URI
+    property_domain : string
+        property domain
+    property_range : string
+        property range
+    definition : string
+        definition string
+    definition_source_name : string
+        definition source name
+    definition_source_uri : string
+        definition source URI
+
+    """
+    import pandas as pd
+
+    from mhdb.owl_boilerplate import safestr
+
+    # equivalentClass and subClassOf:
+    try:
+        equivalent_class_uri, f = safestr(worksheet.equivalentClass[index],
+                                          exclude)
+    except:
+        equivalent_class_uri = None
+    try:
+        subclassof_uri, f = safestr(worksheet.subClassOf[index], exclude)
+    except:
+        subclassof_uri = None
+
+    # Property domain and range:
+    try:
+        property_domain, f = safestr(worksheet.propertyDomain[index], exclude)
+    except:
+        property_domain = None
+    try:
+        property_range, f = safestr(worksheet.propertyRange[index], exclude)
+    except:
+        property_range = None
+
+    # Definition:
+    try:
+        definition, f = safestr(worksheet.Definition[index], exclude)
+    except:
+        definition = None
+
+    # Definition source and link:
+    definition_source_name = None
+    definition_source_uri = None
+    if worksheet2 is not None:
+        try:
+            iref, f = safestr(worksheet.DefinitionReference_index[index], exclude)
             if iref not in exclude:
-                ref = worksheet2.worksheet2Name[worksheet2['index'] == iref]
-                definition += " [{0}]".format(ref)
+                index2 = pd.Index(worksheet2['index']).get_loc(iref)
+                try:
+                    definition_source_name, foo = safestr(
+                        worksheet2.ReferenceName[index2], exclude)
+                except:
+                    definition_source_name = None
+                try:
+                    definition_source_uri, foo = safestr(
+                        worksheet2.ReferenceLink[index2], exclude)
+                except:
+                    definition_source_uri = None
         except:
             pass
 
-    return definition
+    return equivalent_class_uri, subclassof_uri, \
+           property_domain, property_range, \
+           definition, definition_source_name, definition_source_uri
 
 
 def print_header(base_uri, version, label, comment):
@@ -176,187 +279,31 @@ def print_header(base_uri, version, label, comment):
     return header
 
 
-def print_object_properties_header():
+def print_subheader(object_type):
     return """
 #################################################################
-#    Object Properties
+#    {0}
 #################################################################
-"""
+""".format(object_type)
 
 
-def print_object_property(property_uri_stem, label='', comment='',
-                          equivalentURI='', subClassOf_uri='',
-                          domain='', range='', exclude=['']):
+def build_owl_string(uri_stem, owl_type, label, comment=None,
+                     index=None, worksheet=None, worksheet2=None,
+                     equivalent_class_uri=None, subclassof_uri=None,
+                     property_domain=None, property_range=None,
+                     exclude=[None]):
     """
 
     Parameters
     ----------
-    property_uri_stem : string
-        property URI stem
-    label : string
-        label
-    comment : string
-        comment
-    equivalentURI : string
-        equivalent URI
-    subClassOf_uri : string
-        subClassOf URI
-    domain : list of strings
-        domain
-    range : list of strings
-        range
-    exclude : list
-        exclusion list
-
-    Returns
-    -------
-    object_property_string : string
-        owl object property
-
-    """
-
-    object_property_string = """
-### {0}
-:{0} rdf:type owl:ObjectProperty ;
-    rdfs:subPropertyOf :relational_property ;
-    rdf:type owl:FunctionalProperty """.format(label, property_uri_stem)
-
-    label = str(label)
-    if label not in exclude:
-        object_property_string += """;
-        rdfs:label "{0}"^^rdfs:Literal """.format(label)
-
-    comment = str(comment)
-    if comment not in exclude:
-        object_property_string += """;
-        rdfs:comment "{0}"^^rdfs:Literal """.format(comment)
-
-    equivalentURI = str(equivalentURI)
-    if equivalentURI not in exclude:
-        object_property_string += """;
-        owl:equivalentClass [ rdf:type owl:Restriction ;
-                              owl:onProperty <{0}>
-                            ] """.format(equivalentURI)
-
-    subClassOf_uri = str(subClassOf_uri)
-    if subClassOf_uri not in exclude:
-        object_property_string += """;
-        rdfs:subClassOf :{0} """.format(subClassOf_uri)
-
-    domain = str(domain)
-    if domain not in exclude:
-        object_property_string += """;
-     rdfs:domain :{0} """.format(domain)
-
-    range = str(range)
-    if range not in exclude:
-        object_property_string += """;
-     rdfs:range :{0} """.format(range)
-
-    object_property_string += """.
-"""
-
-    return object_property_string
-
-
-def print_data_properties_header():
-    return """
-#################################################################
-#    Data properties
-#################################################################
-"""
-
-
-def print_data_property(property_uri_stem, label='', comment='',
-                        equivalentURI='', subClassOf_uri='',
-                        exclude=['']):
-    """
-
-    Parameters
-    ----------
-    property_uri_stem : string
-        property URI stem
-    label : string
-        label
-    comment : string
-        comment
-    equivalentURI : string
-        equivalent URI
-    subClassOf_uri : string
-        subClassOf URI
-    exclude : list
-        exclusion list
-
-    Returns
-    -------
-    data_property_string : string
-        owl data property
-
-    """
-
-    data_property_string = """
-### {0}
-:{0} rdf:type owl:DatatypeProperty """.format(label, property_uri_stem)
-
-    label = str(label)
-    if label not in exclude:
-        data_property_string += """;
-        rdfs:label "{0}"^^rdfs:Literal """.format(label)
-
-    comment = str(comment)
-    if comment not in exclude:
-        data_property_string += """;
-        rdfs:comment "{0}"^^rdfs:Literal """.format(comment)
-
-    equivalentURI = str(equivalentURI)
-    if equivalentURI not in exclude:
-        data_property_string += """;
-        owl:equivalentClass [ rdf:type owl:Restriction ;
-                              owl:onProperty <{0}>
-                            ] """.format(equivalentURI)
-
-    subClassOf_uri = str(subClassOf_uri)
-    if subClassOf_uri not in exclude:
-        data_property_string += """;
-        rdfs:subClassOf :{0} """.format(subClassOf_uri)
-
-    data_property_string += """.
-    """
-
-    return data_property_string
-
-
-def print_classes_header(title=''):
-    if title:
-        space = ' '
-    else:
-        space = ''
-
-    return """
-#################################################################
-#    {0}{1}Classes
-#################################################################
-""".format(title, space)
-
-
-def print_class(class_uri_stem, label, comment='', index=None,
-                worksheet=None, worksheet2=None,
-                equivalentClass='', subClassOf='', exclude=['']):
-    """
-    This function prints output like:
-
-    :IntellectualDisabilityIntellectualDevelopmentDisorder rdf:type owl:Class ;
-            rdfs:label "Intellectual Disability (Intellectual Development Disorder)"^^rdfs:Literal ;
-            rdfs:comment "None"^^rdfs:Literal ;
-            owl:equivalentClass [ rdf:type owl:Restriction ;
-                                  owl:onProperty <http://purl.bioontology.org/ontology/ICD10CM/F71>
-                                ] ;
-            rdfs:subClassOf :http://www.purl.org/mentalhealth#SchizophreniaSpectrumandOtherPsychoticDisorders .
-
-    Parameters
-    ----------
-    class_uri_stem : string
+    uri_stem : string
         class URI stem
+    owl_type : string
+        owl type, such as:
+        'Class',
+        'ObjectProperty',
+        'DatatypeProperty',
+        'FunctionalProperty'
     label : string
         label
     comment : string
@@ -367,84 +314,91 @@ def print_class(class_uri_stem, label, comment='', index=None,
         spreadsheet worksheet containing properties
     worksheet2 : pandas dataframe
         second worksheet containing references
-    equivalentClass : string
-        equivalentClass (to override worksheet)
-    subClassOf : string
-        subClassOf (to override worksheet)
+    equivalent_class_uri : string
+        equivalentClass URI (override worksheet)
+    subclassof_uri : string
+        subClassOf URI (override worksheet)
+    property_domain : string
+        property domain (override worksheet)
+    property_range : string
+        property range (override worksheet)
     exclude : list
         exclusions
 
     Returns
     -------
-    class_string : string
+    owl_string : string
         owl class string
 
     """
-    from mhdb.owl_boilerplate import get_definition
+    from mhdb.owl_boilerplate import get_cells
 
-    if comment in exclude and index is not None and \
-                    worksheet is not None and \
-                    worksheet2 is not None:
-        comment = get_definition(worksheet=worksheet,
-                                 worksheet2=worksheet2,
-                                 index=index,
-                                 exclude=exclude)
+    # Get worksheet contents:
+    class_uri, subclass_uri, prop_domain, prop_range, \
+    definition, definition_source, definition_uri = get_cells(
+        worksheet, index, worksheet2, exclude)
 
-    # If equivalentClass or subClassOf not provided,
+    if comment in exclude:
+        comment = definition
+
+    # If equivalent_class_uri or subclassof_uri not provided,
     # get from worksheet:
-    if equivalentClass in exclude:
-        if index is not None and worksheet is not None:
-            equivalentClass = str(worksheet.equivalentClass[index])
-            if equivalentClass in exclude:
-                equivalentClass = ''
-        else:
-            equivalentClass = ''
-    if subClassOf in exclude:
-        if index is not None and worksheet is not None:
-            subClassOf = str(worksheet.subClassOf[index])
-            if subClassOf in exclude:
-                subClassOf = ''
-        else:
-            subClassOf = ''
+    if equivalent_class_uri in exclude:
+        equivalent_class_uri = class_uri
+    if subclassof_uri in exclude:
+        subclassof_uri = subclass_uri
 
-    class_string = """
+    # If property_domain or property_range not provided,
+    # get from worksheet:
+    if property_domain in exclude:
+        property_domain = prop_domain
+    if property_range in exclude:
+        property_range = prop_range
 
+    owl_string = """
 ### {0}
-:{0} rdf:type owl:Class """.format(label, class_uri_stem)
+:{1} rdf:type owl:{2} """.format(label, uri_stem, owl_type)
 
     label = str(label)
     if label not in exclude:
-        class_string += """;
+        owl_string += """;
             rdfs:label "{0}"^^rdfs:Literal """.format(label)
 
     if comment not in exclude:
-        class_string += """;
-            rdfs:comment "{0}"^^rdfs:Literal """.format(comment)
+        owl_string += """;
+            rdfs:comment "{0} [from: {1}]"^^rdfs:Literal """.\
+            format(definition, definition_source)
 
-    equivalentClass = str(equivalentClass)
-    if equivalentClass not in exclude:
-        class_string += """;
+    if definition_uri not in exclude:
+        owl_string += """;
+            rdfs:isDefinedBy "{0}"^^rdfs:Literal """.format(definition_uri)
+
+    equivalent_class_uri = str(equivalent_class_uri)
+    if equivalent_class_uri not in exclude:
+        owl_string += """;
             owl:equivalentClass [ rdf:type owl:Restriction ;
                                   owl:onProperty <{0}>
-                                ] """.format(equivalentClass)
+                                ] """.format(equivalent_class_uri)
 
-    subClassOf = str(subClassOf)
-    if subClassOf not in exclude:
-        class_string += """;
-            rdfs:subClassOf :{0} """.format(subClassOf)
+    subclassof_uri = str(subclassof_uri)
+    if subclassof_uri not in exclude:
+        if not subclassof_uri.startswith(':'):
+            subclassof_uri = "<{0}>".format(subclassof_uri)
+        owl_string += """;
+            rdfs:subClassOf {0} """.format(subclassof_uri)
 
-    class_string += """.
+    if property_domain not in exclude:
+        owl_string += """;
+     rdfs:domain :{0} """.format(property_domain)
+
+    if property_range not in exclude:
+        owl_string += """;
+     rdfs:range :{0} """.format(property_range)
+
+    owl_string += """.
 """
 
-    return class_string
-
-
-def print_general_axioms_header():
-    return """
-#################################################################
-#    General axioms
-#################################################################
-"""
+    return owl_string
 
 
 def print_general_axioms(disjoint_classes_list=[]):
