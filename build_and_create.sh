876be33eb1f8fe3b771891d32ea3eb1c296e1070
Jon Clucas <jon.clucas@childmind.org>
2017-06-12 18:06:56 -0400

    bash script to build and run Python code

---
+++ build_and_create.sh
@@ -0,0 +1,19 @@
+# buid_and_create.sh
+#
+# This is a simple BASH script to build and execute mhdb/create_mhdb.
+#
+# Requires 'mentalhealth.xlsx' to be included in one of the following
+# locations:
+# • './data'
+# • './mhdb/data'
+# • '/data'
+#
+# Authors:
+#     - Jon Clucas, 2017 (jon.clucas@childmind.org)
+#
+# Copyright 2017, Child Mind Institute (http://childmind.org),
+# Apache v2.0 License
+
+python3 setup.py build
+python3 setup.py install
+mhdb/create_mhdb
