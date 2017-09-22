5a1c913b05305e2a3753bf3f6f306467a7063b1d
Jon Clucas <jon.clucas@childmind.org>
2017-09-22 12:20:41 -0400

    xml ns

--- mhdb/write_rdf.py
+++ mhdb/write_rdf.py
@@ -292,7 +292,7 @@ def print_header(base_uri, version, label, comment,
 @prefix : <{0}#> .
 @prefix owl: <http://www.w3.org/2002/07/owl#> .
 @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-@prefix xml: <http://www.w3.org/XML/1998/namespace> .
+@prefix xml: <http://www.w3.org/XML/1998/namespace#> .
 @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
 @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
 @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
